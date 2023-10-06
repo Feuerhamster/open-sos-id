c910fa144e8f0f2b7d86ca618cd44ebdf6127c72
Feuerhamster <38376566+Feuerhamster@users.noreply.github.com>
2023-10-06 18:50:12 +0200

    move login to own controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using open_sos_id.Attributes;
+using open_sos_id.Models.ViewModels;
+using open_sos_id.Services;
+
+namespace open_sos_id.Controllers;
+
+[Route("/auth")]
+public class AuthController : Controller
+{
+	private readonly IAuthenticationService _auth;
+
+	public AuthController(IAuthenticationService auth)
+	{
+		_auth = auth;
+	}
+
+	public IActionResult Index() => Redirect("/login");
+
+	[HttpGet("login")]
+	public IActionResult Login() => View(new LoginViewModel());
+
+	[HttpPost("login")]
+	public IActionResult Login(LoginViewModel data)
+	{
+		if (!ModelState.IsValid) return View(new LoginViewModel() { IsValid = false });
+
+		LoginResponse? login = this._auth.LoginUser(data.Username, data.Password);
+
+		if (login == null) return View(new LoginViewModel() { IsValid = false });
+
+		CookieOptions cookieOptions = new() {
+			HttpOnly = true,
+			SameSite = SameSiteMode.Strict
+		};
+
+		this.Response.Cookies.Append(AuthenticationService.COOKIE_IDENTIFIER, login.SessionId, cookieOptions);
+
+		if (login.User.IsAdmin) {
+			return Redirect("/admin");
+		} else {
+			return Redirect("/profiles");
+		}
+	}
+
+	[HttpGet("logout")]
+	[Authorization(isAdmin: false)]
+	public IActionResult Logout()
+	{
+		this._auth.LogoutUser(this.HttpContext.Session.Id);
+
+		this.Response.Cookies.Delete(AuthenticationService.COOKIE_IDENTIFIER);
+
+		return Redirect("/login");
+	}
+
+	[Route("error")]
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,54 +9,9 @@ namespace open_sos_id.Controllers;
 [Route("/")]
 public class HomeController : Controller
 {
-	private readonly ILogger<HomeController> _logger;
+	public HomeController() {}
 
-	private readonly IAuthenticationService _auth;
-
-	public HomeController(ILogger<HomeController> logger, IAuthenticationService auth)
-	{
-		_logger = logger;
-		_auth = auth;
-	}
-
-	public IActionResult Index() => Redirect("/login");
-
-	[HttpGet("login")]
-	public IActionResult Login() => View(new LoginViewModel());
-
-	[HttpPost("login")]
-	public IActionResult Login(LoginViewModel data)
-	{
-		if (!ModelState.IsValid) return View(new LoginViewModel() { IsValid = false });
-
-		LoginResponse? login = this._auth.LoginUser(data.Username, data.Password);
-
-		if (login == null) return View(new LoginViewModel() { IsValid = false });
-
-		CookieOptions cookieOptions = new() {
-			HttpOnly = true,
-			SameSite = SameSiteMode.Strict
-		};
-
-		this.Response.Cookies.Append(AuthenticationService.COOKIE_IDENTIFIER, login.SessionId, cookieOptions);
-
-		if (login.User.IsAdmin) {
-			return Redirect("/admin");
-		} else {
-			return Redirect("/profiles");
-		}
-	}
-
-	[HttpGet("logout")]
-	[Authorization(isAdmin: false)]
-	public IActionResult Logout()
-	{
-		this._auth.LogoutUser(this.HttpContext.Session.Id);
-
-		this.Response.Cookies.Delete(AuthenticationService.COOKIE_IDENTIFIER);
-
-		return Redirect("/login");
-	}
+	public IActionResult Index() => Redirect("/auth/login");
 
 	[Route("error")]
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
